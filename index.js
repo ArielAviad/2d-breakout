dbadf3f5f2444c71b14527c368f7424ee53e0cc6
ArielAviad <aviadrel@gmail.com>
2017-07-23 15:38:06 +0300

    make the ball bounce from the walls

--- index.js
+++ index.js
@@ -3,29 +3,36 @@
  */
 var canvas;
 var ctx;
-var x = undefined,y;
+var x,y;
 var dx = 2,dy = -2;
+var ballRadius = 10;
 
 function init(_canvas) {
     canvas = _canvas;
     ctx = _canvas.getContext("2d");
+    x = _canvas.width/2;
+    y = _canvas.height-30;
+
 }
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
-    if (!x){
-        x = canvas.width/2;
-        y = canvas.height-30;
-    }else{
-        x+=dx;y+=dy;
-    }
+    //if ball bounce to the top or bottom of the canvas
+    if (y+dy < 0 || y+dy > canvas.height)
+        dy = -dy;
+    //if ball bounce to the left or right of the canvas
+    if (x+dx < 0 || x+dx > canvas.width)
+        dx = -dx;
+
+    x+=dx;y+=dy;
+
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
 }
