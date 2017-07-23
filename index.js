660522c8503ee9a5ccc6d037d7e29cf61834c4c2
ArielAviad <aviadrel@gmail.com>
2017-07-23 15:29:13 +0300

    draw the ball and make it move.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 /**
  * Created by ariel on 23/07/17.
  */
+var canvas;
+var ctx;
+var x = undefined,y;
+var dx = 2,dy = -2;
 
-function init(canvas) {
-    var ctx = canvas.getContext("2d");
+function init(_canvas) {
+    canvas = _canvas;
+    ctx = _canvas.getContext("2d");
+}
+
+function drawBall() {
     ctx.beginPath();
-
-    ctx.rect(0,0,20,20);
-    ctx.fillStyle = "#ff0000";
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
-
     ctx.closePath();
+}
 
-    ctx.beginPath();
-
-    ctx.arc(50,50,20,0,Math.PI*(3/2),true);
-    ctx.fillStyle = "green";
-    ctx.stroke();
-
-    ctx.closePath();
-
+function draw() {
+    if (!x){
+        x = canvas.width/2;
+        y = canvas.height-30;
+    }else{
+        x+=dx;y+=dy;
+    }
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawBall();
 }
